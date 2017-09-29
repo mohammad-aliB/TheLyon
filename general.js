b6014652266293de8939703ac345484528bad7ac
Mohammad-Ali <mohammad@Mohammads-MacBook-Pro.local>
2017-09-28 21:07:21 -0400

    a

--- general.js
+++ general.js
@@ -22,10 +22,10 @@ mongodb.MongoClient.connect("mongodb://192.168.206.78:27017/TheLyon", function(e
     // TagDatabase=db.collection("Tag_Store")
     // SearchDatabase=db.collection("Search_Store")
     calendar=db.collection("calendar")
-    schoolDay=db.collection("schoolDay")
+    aschoolDay=db.collection("schoolDay")
 });
                                  //   var document = {"a":"b"};//include exiration later
-                                  schoolDay.insert(document, function(err, records) {
+                                  aschoolDay.insert(document, function(err, records) {
                                         if (err) throw err;             
                                         });
 //schoolDay.update({"date":new Date("2017-9-28")},{$set:{"day":2}},{upsert:true},function(err, result) {});
