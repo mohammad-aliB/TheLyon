e292c0a56c68f254ce081b3a88391b7e3553397b
Mohammad-Ali <mohammad@Mohammads-MacBook-Pro.local>
2017-09-28 22:40:49 -0400

    a

--- general.js
+++ general.js
@@ -27,7 +27,7 @@ dispatcher.staticDirectory('/Static','/TheLyon/TheLyon/staticFiles');
 dispatcher.GetRequest('/',function(req,res){
     test=new Date(calcTime(-4));
     testB=new Date().
-    console.log(new Date(test.getDate()+1))
+    console.log(testB.setDate(test.getDate()+1))
     schoolDay.findOne( {"date": {"$gte": test,"$lte":testB.setDate(test.getDate()+1)}},function(error,result){
         if(result){
     res.end(dots.index({"currentDate":test,"day":result["day"]})); 
