a68b9a7a714c01309c980d602216e5051a19b2af
Mohammad-Ali <mohammad@Mohammads-MacBook-Pro.local>
2017-10-02 18:38:23 -0400

    added scraper

--- general.js
+++ general.js
@@ -40,26 +40,48 @@ dispatcher.GetRequest('/',function(req,res){
     });
     //console.log(test)
 schoolDay.update({"date":new Date("2017-9-28")},{$set:{"day":2}},{upsert:true},function(err, result) {});
-    
 });
                                  //   var document = {"a":"b"};//include exiration later
                            //       aschoolDay.insert(document, function(err, records) {
                             //            if (err) throw err;             
                             //            });
 function calcTime( offset) {
-
     // create Date object for current location
     var d = new Date();
-
     // convert to msec
     // add local time zone offset
     // get UTC time in msec
     var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
-
     // create new Date object for different city
     // using supplied offset
     var nd = new Date(utc + (3600000*offset));
-
     // return time as a string
     return nd.toISOString().split('T')[0];
 }
+function scrapeSchoolDays(){
+    var url = 'http://graph.facebook.com/517267866/?fields=picture';
+
+    http.get(url, function(res){
+        var body = '';
+
+        res.on('data', function(chunk){
+            body += chunk;
+        });
+
+        res.on('end', function(){
+            var response = JSON.parse(body);
+            for (x in response[0]["items"]){
+                if(response[0]["items"]["summary"]=="DAY 1"){
+                schoolDay.update({"date":new Date(response[0]["items"]["start"]["date"])},{$set:{"day":1}},{upsert:true},function(err, result) {});
+                
+                }else if(response[0]["items"]["summery"]=="DAY 2"){
+                schoolDay.update({"date":new Date(response[0]["items"]["start"]["date"])},{$set:{"day":2}},{upsert:true},function(err, result) {});
+                }
+            }
+            console.log("Got a response: ", response);
+        });
+    }).on('error', function(e){
+          console.log("Got an error: ", e);
+    });
+}
+scrapeSchoolDays();
