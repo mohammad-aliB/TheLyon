fc03d2e382a38d3cde04cab8e95819fae5d80e46
Mohammad-Ali <mohammad@Mohammads-MacBook-Pro.local>
2017-10-02 19:20:00 -0400

    a

--- general.js
+++ general.js
@@ -57,7 +57,7 @@ dispatcher.GetRequest('/Logout',function(req,res){
 });
 dispatcher.GetRequest('/Admin',function(req,res){
     if(sessionToken==0){
-        res.end("pls login");
+        res.end("pls <a href=\"/Login\">LOGIN</a>");
     }else{
         var cookies = cookie.parse(req.headers.cookie || '');
         if(cookies.sessionToken==sessionToken){
