43ea54aa4473747162a937d58bcc7be34b491f03
Mohammad-Ali <mohammad@Mohammads-MacBook-Pro.local>
2017-10-02 18:39:57 -0400

    fixed http to https

--- general.js
+++ general.js
@@ -1,7 +1,7 @@
 var dispatcher=require("/TheLyon/Dispatcher/index.js") ;
 var dot = require('dot');
 var dots = dot.process({path: "/TheLyon/TheLyon/"});
-var httpModule=require('http');
+var httpsModule=require('https');
 dispatcher.setUP(80,'0.0.0.0');
 
 var mongodb = require('mongodb');
@@ -62,7 +62,7 @@ function calcTime( offset) {
 function scrapeSchoolDays(){
     var url = 'https://clients6.google.com/calendar/v3/calendars/wlmacci@gmail.com/events?calendarId=wlmacci%40gmail.com&singleEvents=true&timeZone=America%2FToronto&maxAttendees=1&maxResults=1000&sanitizeHtml=true&timeMin=2017-08-27T00%3A00%3A00-04%3A00&timeMax=2018-12-10T00%3A00%3A00-04%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs';
 
-    httpModule.get(url, function(res){
+    httpsModule.get(url, function(res){
         var body = '';
 
         res.on('data', function(chunk){
