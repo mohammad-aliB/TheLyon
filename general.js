3a7b2e083c9bb910e325157de186fa7d85063fc9
Mohammad-Ali <mohammad@Mohammads-MacBook-Pro.local>
2017-10-04 22:02:01 -0400

    a

--- general.js
+++ general.js
@@ -25,8 +25,8 @@ dispatcher.GetRequest('/',function(req,res){
         if(!result){
             result=[];
         }
-    var d= new Date();
-    var calendarURL="/Calendar/"+d.getYear()+"/"+d.getMonth();
+        if(test.getMonth()==1){month="January"}else if(test.getMonth()==2){month="Febuary"}else if(test.getMonth==3){month="March"}else if(test.getMonth()==4){month="April"}else if(test.getMonth()==5){month="May"}else if(test.getMonth()==6){month="June"}else if(test.getMonth()==7){month="July"}else if(test.getMonth()==8){month="Augest"}else if(test.getMonth()==9){month="September"}else if(test.getMonth()==10){month="October"}else if(test.getMonth()==11){month="November"}else{month="December"}
+    var calendarURL="/Calendar/"+test.getFullYear()+"/"+month;
         res.end(dots.index({"currentDate":test,"day":result["day"],"calendarURL":calendarURL})); 
     });
 });
